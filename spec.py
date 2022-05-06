6add1074aafe9b0e073f8d1499de15aad132856f
weichan <wei-bin.chan@keysight.com>
2022-05-06 13:53:10 +0800

    Add spec framework

---
+++ spec.py
@@ -0,0 +1,70 @@
+# [Balsa]
+balsaSpec = {
+    "PowerSupplySpec":
+    {
+        "Current":
+        {
+            "High": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 },
+            "Low": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 }},
+
+        "Voltage":
+        {
+            "High": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 },
+            "Low": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 }}},
+
+    "EloadSpec":
+    {
+        "Current":
+        {
+            "High": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 },
+            "Mid": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 },
+            "Low": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 }},
+
+        "Voltage":
+        {
+            "High": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 },
+            "Mid": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 },
+            "Low": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 }},
+        "Power":
+        {
+            "High": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 },
+            "Mid": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 },
+            "Low": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 }},
+
+        "Resistance":
+        {
+            "High": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 },
+            "Mid": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 },
+            "Low": {
+                "Gain"      :   0.00001,
+                "Offset"    :   0.0001 }}}}
