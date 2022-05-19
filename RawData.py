ad143871ab85f827e2837867fb43d1ce18a860e3
weichan <wei-bin.chan@keysight.com>
2022-05-19 14:24:49 +0800

    osysy

---
+++ RawData.py
@@ -0,0 +1,170 @@
+import os, sys, re, argparse
+import pandas as pd
+from datetime import datetime
+from openpyxl import load_workbook
+import Unit_TestInfo as utif
+
+# IO file settings, currently not in use
+user = 'weichan'
+#csv_file_loc = "C:\\Users\\{0}\\Downloads\\dataAutomation\\RAW_DATA\\".format(user)
+csv_file_loc = os.getcwd() + "\\"
+#output_file_loc = "C:\\Users\\{0}\\Downloads\\dataAutomation\\Output\\".format(user)
+output_file_loc = os.getcwd() + "\\"
+output_file_name = "outputfile-" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".xlsx"
+output_full_path = output_file_loc + output_file_name
+
+# dataFrame settings
+compiledresultDF = pd.DataFrame()
+
+scriptName = sys.argv[0]
+parser = argparse.ArgumentParser(description=str("Inputing arguments for " + scriptName))
+parser.add_argument('-id', metavar="ID", help="Key in four digit ID")
+parser.add_argument('-csv', metavar="CSV", help="CSV files folder location")
+parser.add_argument('-output', metavar="Output file location", help="Location of file data to add in")
+parser.add_argument('-file', metavar="Output file name")
+parser.add_argument('-user', metavar="Username", help="User PC name")
+parser.add_argument('-args1', metavar="Column1 Name", default="Name")
+parser.add_argument('-args2', metavar="Column2 Name", default="Result")
+parser.add_argument('-args3', metavar="Column3 Name", default="PercentSpec")
+parser.add_argument('-args4', metavar="Column4 Name", default="UpperLimit,LowerLimit,Parent3") # those col that only occurs 1 time
+parser.add_argument('-args5', metavar="Column5 Name", default="Parent4") # cols that occur multiple times
+parser.add_argument('-pt', metavar="Sheet Name", default="Sheet1", help="Temp workaround")
+
+args = parser.parse_args()
+
+if args.id and args.id.isdigit():
+    id = int(args.id)
+else:
+    id = 0
+    # print("ID is null or non-digit, generating ID: {:04d}".format(id))
+
+if args.csv:
+    csv_file_loc = args.csv
+else:
+    pass#raise Exception("Please insert raw data file location")
+
+if args.output:
+    output_file_loc = args.output
+
+if args.file:
+    output_file_name = args.file
+    if output_file_name.endswith('.xlsx'):
+        pass
+    else:
+        output_file_name += '.xlsx'
+
+if args.user:
+    user = args.user
+
+if args.pt:
+    sheetName = args.pt
+
+args1, args2, args3 = args.args1, args.args2, args.args3
+args4 = []
+if args.args4:
+    args4 = args.args4.split(",")
+
+def check_file_loc():
+
+    global output_full_path
+
+    try:
+        if not os.path.exists(csv_file_loc):
+            os.makedirs(csv_file_loc)
+        if not os.path.exists(output_file_loc):
+            os.makedirs(output_file_loc)
+
+        output_full_path = output_file_loc + output_file_name
+
+    except Exception as e:
+        raise Exception("Error at check_file_loc : " + str(e))
+        sys.exit(1)
+
+
+
+def createDataFrame():
+
+    try:
+        resultDFList = [] #list
+
+        files = [x for x in os.listdir(csv_file_loc) if x.endswith(".csv")]
+        if not files:
+            print(f"File not found in the folder: {csv_file_loc} ...\nExitting!")
+            sys.exit(1)
+
+        for file in range(len(files)):
+            os.chdir(csv_file_loc)
+            print(f"Extracting data from [{files[file]}] ...")
+            resultDFList.append(pd.read_csv(files[file]))
+            argslist = [args1, args2, args3]
+            resultDFList[file] = resultDFList[file][argslist]
+
+            # workaround, ToDO: use regex to extract unit SN as ResultName
+            resultName = args2 + str(file)
+            percentName = args3 + str(file)
+            resultDFList[file] = resultDFList[file].set_index(args1)
+            resultDFList[file] = resultDFList[file].rename(columns={args2:resultName, args3:percentName})
+
+        # compile df list into signle df
+        compileDF = resultDFList[0]
+        for file in range(len(files)-1):
+            compileDF = compileDF.join(resultDFList[file+1])
+
+        # args4 for UL/LL etc, only appear one time in df
+        if args4:
+            args4.insert(0, args1)
+            args4DF = pd.read_csv(files[0])
+            args4DF = args4DF[args4]
+            print("Appended:\n" + args4DF)
+            args4DF = args4DF[args4]
+            args4DF = args4DF.set_index(args1)
+            compileDF = compileDF.join(args4DF)
+
+        # rearrage df
+        sortCols = compileDF.columns.tolist()
+        sortCols.sort()
+        compileDF = compileDF[sortCols]
+
+        return compileDF
+
+    except Exception as e:
+        raise Exception("Error at createDataFrame: " + str(e))
+        sys.exit(1)
+
+
+
+def WriteExcel(compiledresultDF):
+    try:
+        print(f" ================ Output File : {output_file_name} ================ ")
+        print(f"File location : {output_full_path}\n")
+
+        print(f"Checking if {output_file_name} exists ...")
+        if not os.path.exists(output_full_path):
+            print("File does not exist, creating new excel file now ...")
+            writer = pd.ExcelWriter(output_full_path, engine='xlsxwriter')
+            compiledresultDF.to_excel(writer, sheet_name=sheetName, index=True, header=True)
+        # append excel sheet if excel exists
+        else:
+            print("File exists, appending data to exising file ...")
+            excelWorkbook = load_workbook(output_full_path)
+            writer = pd.ExcelWriter(output_full_path, engine='openpyxl')
+            writer.book = excelWorkbook
+            compiledresultDF.to_excel(writer, sheet_name=sheetName, index=True, header=True)
+        writer.save()
+        print("... done\n")
+
+    except Exception as e:
+        raise Exception("Error at WriteExcel: " + str(e))
+
+
+
+if __name__ == '__main__':
+    try:
+        check_file_loc()
+        compiledresultDF = createDataFrame()
+        WriteExcel(compiledresultDF)
+        sys.exit(0)
+
+    except Exception as e:
+        print(e)
+        sys.exit(1)
---
+++ Unit_TestInfo.py
@@ -0,0 +1,25 @@
+import re, os, sys
+
+class unitInfo:
+
+    @staticmethod
+    def get_unit_SN():
+        pass
+
+
+    @classmethod
+    def get_model(unitInfo):
+        pass
+
+
+
+class testInfo:
+
+    @staticmethod
+    def get_PT_name():
+        print("Feature not supported, currently PT is not included in output data ...")
+        sys.exit(1)
+
+    @staticmethod
+    def get_test_temp():
+        pass
