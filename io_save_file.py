a690f49216dad04f4fe56178c71cd300b33baa6f
weichan <wei-bin.chan@keysight.com>
2022-07-21 13:36:27 +0800

    add files and func

---
+++ io_save_file.py
@@ -0,0 +1,53 @@
+from fileinput import filename
+import pandas as pd 
+import matplotlib.pyplot as plt
+from openpyxl import Workbook, load_workbook
+import os, re, sys
+
+
+# * ================================================ Sanity Check ================================================
+
+
+# * =============================================== Save Dataframe =============================================
+
+def dataframe_to_excel(df, output_full_path, sheetName="Sheet1"):
+    try:
+        output_file_name = output_full_path.split("\\")[-1]
+        print(f" ================ Output File : {output_file_name} ================ ")
+        print(f"File location : {output_full_path}\n")
+
+        print(f"Checking if {output_file_name} exists ...")
+        if not os.path.exists(output_full_path):
+            print("File does not exist, creating new excel file now ...")
+            writer = pd.ExcelWriter(output_full_path, engine='xlsxwriter')
+            df.to_excel(writer, sheet_name=sheetName, index=True, header=True)
+        # append excel sheet if excel exists
+        else:
+            print("File exists, appending data to exising file ...")
+            excelWorkbook = load_workbook(output_full_path)
+            writer = pd.ExcelWriter(output_full_path, engine='openpyxl')
+            writer.book = excelWorkbook
+            df.to_excel(writer, sheet_name=sheetName, index=True, header=True)
+        writer.save()
+        return print("... done\n")
+        
+
+    except Exception as e:
+        raise Exception("Error at WriteExcel: " + str(e))
+
+
+
+def append_to_sheet(df, output_full_path, sheetName):
+    excelWorkbook = load_workbook(output_full_path)
+    writer = pd.ExcelWriter(output_full_path, engine='openpyxl')
+    writer.book = excelWorkbook
+    df.to_excel(writer, sheet_name=sheetName, index=True, header=True)
+    writer.save()
+    print("saving file ...")
+
+
+def create_excel_file(full_path):
+    wb = Workbook(full_path)
+    filename = full_path.split("\\")[-1]
+    wb.save(filename)
+    return
